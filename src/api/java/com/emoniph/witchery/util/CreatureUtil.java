29f9d70ef5ca97ebe0ae8333d87178a93bf50e2a
SpitefulFox <diamondjbj2@yahoo.com>
2017-11-09 20:50:38 -0800

    Make Witchery Support Compile

---
+++ src/api/java/com/emoniph/witchery/util/CreatureUtil.java
@@ -0,0 +1,9 @@
+package com.emoniph.witchery.util;
+
+import net.minecraft.entity.Entity;
+
+public class CreatureUtil {
+    public static boolean isVampire(Entity ent){
+        return false;
+    }
+}
