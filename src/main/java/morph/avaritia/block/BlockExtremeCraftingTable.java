5c3c99b9f427c295c0827a592d4b3514eaaeb181
p455w0rd <p455w0rd@gmail.com>
2018-07-04 10:20:55 -0400

    Fix #70

--- src/main/java/morph/avaritia/block/BlockExtremeCraftingTable.java
+++ src/main/java/morph/avaritia/block/BlockExtremeCraftingTable.java
@@ -1,5 +1,7 @@
 package morph.avaritia.block;
 
+import javax.annotation.Nullable;
+
 import morph.avaritia.Avaritia;
 import morph.avaritia.api.registration.IModelRegister;
 import morph.avaritia.tile.TileDireCraftingTable;
@@ -11,6 +13,9 @@ import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.client.renderer.block.statemap.StateMapperBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.stats.StatList;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumBlockRenderType;
 import net.minecraft.util.EnumFacing;
@@ -23,47 +28,66 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class BlockExtremeCraftingTable extends BlockContainer implements IModelRegister {
 
-    public BlockExtremeCraftingTable() {
-        super(Material.IRON);
-        setHardness(50F);
-        setResistance(2000F);
-        setUnlocalizedName("avaritia:extreme_crafting_table");
-        setHarvestLevel("pickaxe", 3);
-        setSoundType(SoundType.GLASS);
-        setCreativeTab(Avaritia.tab);
-        setRegistryName("extreme_crafting_table");
-    }
+	public BlockExtremeCraftingTable() {
+		super(Material.IRON);
+		setHardness(50F);
+		setResistance(2000F);
+		setUnlocalizedName("avaritia:extreme_crafting_table");
+		setHarvestLevel("pickaxe", 3);
+		setSoundType(SoundType.GLASS);
+		setCreativeTab(Avaritia.tab);
+		setRegistryName("extreme_crafting_table");
+	}
 
-    @Override
-    public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, EnumFacing side, float hitX, float hitY, float hitZ) {
-        if (world.isRemote) {
-            return true;
-        } else {
-            player.openGui(Avaritia.instance, 1, world, pos.getX(), pos.getY(), pos.getZ());
-            return true;
-        }
-    }
+	@Override
+	public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, EnumFacing side, float hitX, float hitY, float hitZ) {
+		if (world.isRemote) {
+			return true;
+		}
+		else {
+			player.openGui(Avaritia.instance, 1, world, pos.getX(), pos.getY(), pos.getZ());
+			return true;
+		}
+	}
 
-    @Override
-    public TileEntity createNewTileEntity(World world, int meta) {
-        return new TileDireCraftingTable();
-    }
+	@Override
+	public TileEntity createNewTileEntity(World world, int meta) {
+		return new TileDireCraftingTable();
+	}
 
-    @Override
-    public EnumBlockRenderType getRenderType(IBlockState state) {
-        return EnumBlockRenderType.MODEL;
-    }
+	@Override
+	public EnumBlockRenderType getRenderType(IBlockState state) {
+		return EnumBlockRenderType.MODEL;
+	}
 
-    @Override
-    @SideOnly (Side.CLIENT)
-    public void registerModels() {
-        ModelResourceLocation loc = new ModelResourceLocation("avaritia:crafting", "type=extreme");
-        ModelLoader.setCustomStateMapper(this, new StateMapperBase() {
-            @Override
-            protected ModelResourceLocation getModelResourceLocation(IBlockState state) {
-                return loc;
-            }
-        });
-        ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(this), 0, loc);
-    }
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void registerModels() {
+		ModelResourceLocation loc = new ModelResourceLocation("avaritia:crafting", "type=extreme");
+		ModelLoader.setCustomStateMapper(this, new StateMapperBase() {
+			@Override
+			protected ModelResourceLocation getModelResourceLocation(IBlockState state) {
+				return loc;
+			}
+		});
+		ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(this), 0, loc);
+	}
+
+	@Override
+	public void harvestBlock(World worldIn, EntityPlayer player, BlockPos pos, IBlockState state, @Nullable TileEntity te, @Nullable ItemStack stack) {
+		player.addStat(StatList.getBlockStats(this));
+		player.addExhaustion(0.025F);
+		stack = getItemBlockWithNBT(te);
+		spawnAsEntity(worldIn, pos, stack);
+	}
+
+	private ItemStack getItemBlockWithNBT(@Nullable TileEntity te) {
+		ItemStack stack = new ItemStack(this);
+		NBTTagCompound nbttagcompound = new NBTTagCompound();
+		if (te != null) {
+			te.writeToNBT(nbttagcompound);
+			stack.setTagInfo("BlockEntityTag", nbttagcompound);
+		}
+		return stack;
+	}
 }
